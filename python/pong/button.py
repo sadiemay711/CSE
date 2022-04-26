a421aa1fdd1579eb76cf923a1d33d25f2f980954
Sadie May <77846895+sadiemay711@users.noreply.github.com>
2022-04-26 13:43:02 -0500

    make a clickable button or smth

--- python/pong/button.py
+++ python/pong/button.py
@@ -4,10 +4,20 @@ BLACK = (0,0,0)
 
 
 class Button(pygame.sprite.Sprite):
-    def __init__(self, color, width, height):
-        super().__init__()
+    def __init__(self, x, y,width,height, color):
+        super().__init__() 
         self.image = pygame.Surface([width, height])
         self.image.fill(BLACK)
-        self.image.set_colorkey (BLACK)
+        self.image.set_colorkey(BLACK)
+
         pygame.draw.rect(self.image, color, [0, 0, width, height])
-        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect(center = (x, y))
+
+        self.clicked = False
+
+    def update(self, event_list):
+        for event in event_list:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if self.rect.collidepoint(event.pos):
+                    self.clicked = not self.clicked
+      #  return(self.clicked)
--- python/pong/menu.py
+++ python/pong/menu.py
@@ -7,43 +7,35 @@ from button import Button
 pygame.init() 
 clock = pygame.time.Clock()
 
-# screen resolution 
-res = (720,720) 
+hoz,vert = (720,720) 
 # "I feel like a jellyfish sliding off a car"
 # opens up a window 
-screen = pygame.display.set_mode(res) 
-  
-# white color 
-color = (255,255,255) 
-  
-# light shade of the button 
+screen = pygame.display.set_mode(size=(hoz,vert)) 
+WHITE = (255,255,255) 
 color_light = (170,170,170) 
-  
-# dark shade of the button 
 color_dark = (100,100,100) 
-  
-# stores the width of the 
-# screen into a variable 
 width = screen.get_width() 
-  
-# stores the height of the 
-# screen into a variable 
 height = screen.get_height() 
-  
-# defining a font 
+
+buttonA = Button(100,100, 50,50, WHITE)
+
+
 smallfont = pygame.font.SysFont(None,35)
 
+all_sprites_list = pygame.sprite.Group()
+all_sprites_list.add(buttonA)
 # rendering a text written in
 # this font
-text = smallfont.render('Quit' , True , color)
+text = smallfont.render('Quit' , True , WHITE)
 run = True
 while run:
+    mouse = pygame.mouse.get_pos() 
     clock.tick(60)
     event_list = pygame.event.get()
     for event in event_list:
         if event.type == pygame.QUIT:
             run = False 
-
-    # updates the frames of the game 
+    all_sprites_list.draw(screen)
+    buttonA.update(event_list)
     pygame.display.update()
-    print(pygame.mouse.get_pos()) 
+    print(buttonA.clicked) 
